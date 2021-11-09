a9ee39247dfdcb58cddc3aa5b8042e8860b103f5
laa7352 <laa7352@hotmail.com>
2021-11-09 20:01:08 +0800

    server support multi clients

--- client/client.py
+++ client/client.py
@@ -1,16 +1,32 @@
 #!/usr/bin/python3
 
 import socket
+import sys
+import threading
 
 HOST = '127.0.0.1'
 PORT = 1102
-clientMessage = 'Hello!'
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((HOST, PORT))
-client.sendall(clientMessage.encode())
+# TODO verify HOST' ip by command line argv
+if len(sys.argv) > 1:
+  try:
+    socket.inet_aton(sys.argv[1])
+    # legal
+    HOST = sys.argv[1]
+  except socket.error:
+    print('ip format got error, use default ip: ' + HOST)
 
-serverMessage = str(client.recv(1024), encoding='utf-8')
-print('Server:', serverMessage)
+def connectSocket():
+  #clientMessage = 'Hello!'
+  client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  client.connect((HOST, PORT))
+  #client.sendall(clientMessage.encode())
 
-client.close()
+  while True:
+    serverMessage = str(client.recv(1024), encoding='utf-8')
+    print('Server:', serverMessage)
+
+
+socket_t = threading.Thread(target = connectSocket)
+socket_t.start();
+#client.close()
--- server/server.py
+++ server/server.py
@@ -1,19 +1,60 @@
 #!/usr/bin/python3
 
 import socket
+import threading
+import fileinput
+import threading
+#import time
+#import os
+
 HOST = ''
 PORT = 1102
+client_list = []
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((HOST, PORT))
-server.listen(10)
+class SockerServerThread(threading.Thread):
+  def __init__(self,daemon=None):
+    super(SockerServerThread,self).__init__(daemon=daemon)
+    self.__is_running = True
+    self.daemon = daemon
 
+  def terminate(self):
+    self.__is_running = False
+
+  def run(self):
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server.bind((HOST, PORT))
+    server.listen(10)
+    print("Socket Server listening")
+    while True:
+      conn, addr = server.accept()
+      print('Client is connected', conn, addr);
+      client_list.append(conn)
+      print(client_list)
+
+mSocketServer = SockerServerThread();
+mSocketServer.start()
+########################################################################
+# TODO
+#   simulator send message to clients
 while True:
-    conn, addr = server.accept()
-    clientMessage = str(conn.recv(1024), encoding='utf-8')
+  message = input()
+  if message == 'exit':
+    break
+  elif message == 'list':
+    print(client_list)
+  else:
+    print(message)
+    for client in client_list:
+       try:
+         client.sendall(message.encode())
+       except Exception as e:
+         print('remove client when got error')
+         print(client)
+         client_list.remove(client)
 
-    print('Client message is:', clientMessage)
+########################################################################
 
-    serverMessage = 'I\'m here!'
-    conn.sendall(serverMessage.encode())
-    conn.close()
+mSocketServer.terminate()
+
+print("DONE")
