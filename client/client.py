e99d3d8024d0a2433f8907458c69f605f6a5ce81
laa7352 <laa7352@hotmail.com>
2021-11-17 19:57:37 +0800

    add client send function

--- client/client.py
+++ client/client.py
@@ -3,38 +3,85 @@
 import socket
 import sys
 import threading
+import fileinput
 
-HOST = '127.0.0.1'
-PORT = 1102
+class ClientSocketThread(threading.Thread):
+    DefaultHOST = '127.0.0.1'
+    DefaultPort = 1102
+    def __init__(self, daemon=None, HOST=DefaultHOST, PORT=DefaultPort):
+        super(ClientSocketThread, self).__init__(daemon=daemon)
+        self.__is_running = True
+        self.daemon = daemon
+        self.HOST = HOST
+        self.PORT = PORT
+        self.socket = None
+        self.__is_running = True
 
-# TODO verify HOST' ip by command line argv
-if len(sys.argv) > 1:
-  try:
-    socket.inet_aton(sys.argv[1])
-    # legal
-    HOST = sys.argv[1]
-  except socket.error:
-    print('ip format got error, use default ip: ' + HOST)
+    def terminate(self):
+        if self.socket != None:
+            self.socket.close()
 
-def connectSocket():
-  #clientMessage = 'Hello!'
-  client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  client.connect((HOST, PORT))
-  #client.sendall(clientMessage.encode())
+        self.socket = None
+        self.__is_running = False
 
-  while True:
-    try:
-        serverMessage = str(client.recv(1024), encoding='utf-8')
-        if len(serverMessage) > 0:
-          print('Server:', serverMessage)
+    def connect(self):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.connect((self.HOST, self.PORT))
+
+    def receive(self):
+        while True:
+            try:
+                serverMessage = str(self.socket.recv(1024), encoding='utf-8')
+                if len(serverMessage) > 0:
+                    print('Server:', serverMessage)
+                else:
+                    print('Receive error, len:', len(serverMessage))
+                    break
+            except socket.error as e:
+                print ("Error receiving data: %s" %  e)
+                break
+
+    def sendall(self, message):
+        if self.socket != None:
+            self.socket.sendall(message)
+
+    def run(self):
+        self.connect()
+        self.receive()
+
+if __name__ == '__main__':
+
+    if len(sys.argv) == 3:
+        try:
+            socket.inet_aton(sys.argv[1])
+            # legal
+            HOST = sys.argv[1]
+            PORT = int(sys.argv[2])
+        except socket.error:
+            print('ip format got error, use default ip: ' + HOST)
+            HOST = '127.0.0.1'
+            PORT = 1102
+    else:
+        HOST = '127.0.0.1'
+        PORT = 1102
+
+    mClientSocket = ClientSocketThread(True, HOST, PORT)
+    mClientSocket.start()
+    while True:
+        message = input()
+        if message == 'exit':
+            mClientSocket.terminate()
+            break
+        elif message == 'restart':
+            mClientSocket = ClientSocketThread(True, HOST, PORT)
+            mClientSocket.start()
+        elif message == 'disconnect':
+            mClientSocket.terminate()
         else:
-          print('Receive error, len:', len(serverMessage))
-          break
-    except socket.error as e:
-        print ("Error receiving data: %s",  e)
-        break
+            mClientSocket.sendall(message.encode())
+        continue
 
+    print("DONE")
 
-socket_t = threading.Thread(target = connectSocket)
-socket_t.start();
-#client.close()
+    exit()
+
--- server/server.py
+++ server/server.py
@@ -7,56 +7,87 @@ import threading
 #import time
 #import os
 
-HOST = ''
-PORT = 1102
-client_list = []
+#HOST = ''
+#PORT = 1102
+#client_list = []
 
-class SockerServerThread(threading.Thread):
-  def __init__(self,daemon=None):
-    super(SockerServerThread,self).__init__(daemon=daemon)
-    self.__is_running = True
-    self.daemon = daemon
+class ServerSocketThread(threading.Thread):
+    def __init__(self, daemon=None):
+        super(ServerSocketThread, self).__init__(daemon=daemon)
+        self.__is_running = True
+        self.daemon = daemon
+        self.client_list = []
+        self.HOST = ''
+        self.PORT = 1102
 
-  def terminate(self):
-    self.__is_running = False
+    def printList(self):
+        print(self.client_list)
 
-  def run(self):
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server.bind((HOST, PORT))
-    server.listen(10)
-    print("Socket Server listening")
+    def broadcast(self, message):
+        remove_client_list = []
+        for client in self.client_list:
+            try:
+                client.sendall(message)
+            except Exception as e:
+                print('remove client when got error')
+                print(client)
+                remove_client_list.append(client)
+
+        for client in remove_client_list:
+            self.client_list.remove(client)
+
+    def terminate(self):
+        for client in self.client_list:
+            client.close()
+        self.__is_running = False
+
+
+    def listenToClient(self, client, address):
+        size = 1024
+        print('listen client %d ' % client.fileno())
+        while True:
+            try:
+                message = str(client.recv(1024), encoding='utf-8')
+                if message:
+                    # Set the response to echo back the recieved data 
+                    #response = data
+                    #print('Client %d: %s' %client.fileno(), message)
+                    print('{} {}{} {}'.format('Client', client.fileno(), ':', message))
+                else:
+                    raise error('Client disconnected')
+            except:
+                client.close()
+                return False
+
+    def run(self):
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        server.bind((self.HOST, self.PORT))
+        server.listen(10)
+        print("Socket Server listening")
+        while True:
+            client, address = server.accept()
+            print('Client is connected', client, address)
+            self.client_list.append(client)
+            print(self.client_list)
+            threading.Thread(target = self.listenToClient, args = (client, address)).start()
+
+
+if __name__ == '__main__':
+    mServerSocket = ServerSocketThread(True)
+    mServerSocket.start()
+    ########################################################################
+    # TODO
+    #   simulator send message to clients
     while True:
-      conn, addr = server.accept()
-      print('Client is connected', conn, addr);
-      client_list.append(conn)
-      print(client_list)
+        message = input()
+        if message == 'exit':
+            mServerSocket.terminate()
+            break
+        elif message == 'list':
+            mServerSocket.printList();
+        else:
+            mServerSocket.broadcast(message.encode())
 
-mSocketServer = SockerServerThread();
-mSocketServer.start()
-########################################################################
-# TODO
-#   simulator send message to clients
-while True:
-  message = input()
-  if message == 'exit':
-    break
-  elif message == 'list':
-    print(client_list)
-  else:
-    for client in client_list:
-       try:
-         client.sendall(message.encode())
-       except Exception as e:
-         print('remove client when got error')
-         print(client)
-         client_list.remove(client)
-
-########################################################################
-
-
-for client in client_list:
-  client.close()
-mSocketServer.terminate()
-
-print("DONE")
+    ########################################################################
+    print("DONE")
